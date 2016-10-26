8b02fcee10ef97776b8131baa5dca2ebb1b758f2
jason0539 <jason0539@163.com>
2016-10-26 19:42:43 +0800

    初始化工程

---
+++ app/src/main/java/com/tuotuo/jamlab/JamApplication.java
@@ -0,0 +1,11 @@
+package com.tuotuo.jamlab;
+
+import android.app.Application;
+
+/**
+ * Created by liuzhenhui on 2016/10/26.
+ */
+public class JamApplication extends Application {
+    public static final String TAG = JamApplication.class.getSimpleName();
+
+}
