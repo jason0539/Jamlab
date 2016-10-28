ef45b307946286f16453cd6098d74d0fe6ca75bd
liuzhenhui <liuzhenhui@tuotuonet.com>
2016-10-28 18:52:59 +0800

    feature:工具类完善

--- app/src/main/java/com/tuotuo/jamlab/common/utils/FileUtils.java
+++ app/src/main/java/com/tuotuo/jamlab/common/utils/FileUtils.java
@@ -1,12 +1,24 @@
 package com.tuotuo.jamlab.common.utils;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.math.BigInteger;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.security.MessageDigest;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 /**
  * Created by liuzhenhui on 2016/10/28.
@@ -14,7 +26,8 @@ import java.security.MessageDigest;
 public class FileUtils {
     public static final String TAG = FileUtils.class.getSimpleName();
 
-    public static String getMd5ByFile(File file) {
+
+    public static String getFileMd5(File file) {
         String value = null;
         FileInputStream in = null;
         try {
@@ -36,5 +49,93 @@ public class FileUtils {
             }
         }
         return value;
+    }
+
+    public static void closeIO(Closeable... closeables) {
+        if (null == closeables || closeables.length <= 0) {
+            return;
+        }
+        for (Closeable cb : closeables) {
+            try {
+                if (null == cb) {
+                    continue;
+                }
+                cb.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void copyFile(InputStream in, OutputStream out) {
+        try {
+            byte[] b = new byte[2 * 1024 * 1024]; //2M memory
+            int len = -1;
+            while ((len = in.read(b)) > 0) {
+                out.write(b, 0, len);
+                out.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            closeIO(in, out);
+        }
+    }
+
+    public static void copyFileFast(File in, File out) {
+        FileChannel filein = null;
+        FileChannel fileout = null;
+        try {
+            filein = new FileInputStream(in).getChannel();
+            fileout = new FileOutputStream(out).getChannel();
+            filein.transferTo(0, filein.size(), fileout);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            closeIO(filein, fileout);
+        }
+    }
+
+    public static void zip(InputStream is, OutputStream os) {
+        GZIPOutputStream gzip = null;
+        try {
+            gzip = new GZIPOutputStream(os);
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = is.read(buf)) != -1) {
+                gzip.write(buf, 0, len);
+                gzip.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            closeIO(is, gzip);
+        }
+    }
+
+    public static void unzip(InputStream is, OutputStream os) {
+        GZIPInputStream gzip = null;
+        try {
+            gzip = new GZIPInputStream(is);
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = gzip.read(buf)) != -1) {
+                os.write(buf, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            closeIO(gzip, os);
+        }
+    }
+
+    public static void downloadFile(Context context, String fileurl) {
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(fileurl));
+        request.setDestinationInExternalPublicDir("/Download/", fileurl.substring(fileurl.lastIndexOf("/") + 1));
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        downloadManager.enqueue(request);
     }
 }
