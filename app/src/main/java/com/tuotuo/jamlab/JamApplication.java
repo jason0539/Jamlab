a0785d8a1a6b90610cae1ab1b41c667736622e7a
liuzhenhui <liuzhenhui@tuotuonet.com>
2016-10-31 15:01:07 +0800

    feature:stetho

--- app/src/main/java/com/tuotuo/jamlab/JamApplication.java
+++ app/src/main/java/com/tuotuo/jamlab/JamApplication.java
@@ -2,6 +2,7 @@ package com.tuotuo.jamlab;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
 import com.squareup.leakcanary.LeakCanary;
 
 import io.realm.Realm;
@@ -15,16 +16,22 @@ public class JamApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
         initThirdPartLibrary();
     }
 
     private void initThirdPartLibrary() {
+        if (BuildConfig.DEBUG) {
+            if (LeakCanary.isInAnalyzerProcess(this)) {
+                // This process is dedicated to LeakCanary for heap analysis.
+                // You should not init your app in this process.
+                return;
+            }
+            LeakCanary.install(this);
+            Stetho.initializeWithDefaults(this);
+            Stetho.newInitializerBuilder(this)
+                    .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                    .build();
+        }
         Realm.init(this);
     }
 }
